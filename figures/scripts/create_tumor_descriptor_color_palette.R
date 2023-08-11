713571b7e6263d4939ece528451714399a1d53aa
antoniachroni <chronia@chop.edu>
2023-08-11 19:00:05 -0400

    Replace color names with hex codes

--- figures/scripts/create_tumor_descriptor_color_palette.R
+++ figures/scripts/create_tumor_descriptor_color_palette.R
@@ -20,10 +20,10 @@ if (!dir.exists(output_dir)) {
 
 
 ### A color scale for tumor_descriptor data
-tumor_descriptor_col_palette <- c("dodgerblue3", 
-                                  "gray", 
+tumor_descriptor_col_palette <- c("#1e90ff", 
+                                  "#808080", 
                                   "#800080", 
-                                  "firebrick3",
+                                  "#cd2626",
                                   "#FDB863",
                                   "#A6611A")
 
@@ -43,15 +43,15 @@ tumor_descriptor_df <- data.frame(color_names = tumor_descriptor_col_names,
 ###-----------------------------------------
 ### A color scale for tumor_descriptor data for creating corplots
 # `timepoint' stands for any of the following: "Diagnosis", "Progressive", "Recurrence"
-timepoint_col_palette <- c("dodgerblue3", 
-                                  "firebrick3",
-                                  "gray34",
-                                  "#DFC27D")
+timepoint_col_palette <- c("#1e90ff",
+                           "#cd2626",
+                           "#575757",
+                           "#DFC27D")
 
 timepoint_col_names <- c("timepoint",
-                                "Deceased",
-                                "Common",
-                                "Uncommon")
+                         "Deceased",
+                         "Common",
+                         "Uncommon")
 
 # Format as data.frame
 timepoint_df <- data.frame(color_names = timepoint_col_names,
